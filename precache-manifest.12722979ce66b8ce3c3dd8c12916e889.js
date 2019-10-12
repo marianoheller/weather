1ff67d92b929bcf9e88b1d75ec32b9f1981fb2e3
Mariano Mariano <marianofheller@gmail.com>
2019-10-12 10:55:07 -0300

    Updates

---
+++ precache-manifest.12722979ce66b8ce3c3dd8c12916e889.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54da9a78ead9a516bfc4b3f64a87526e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "803e7fc10f0235442f62",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9111e0724bd291de345e",
+    "url": "/static/js/2.2f17e02d.chunk.js"
+  },
+  {
+    "revision": "803e7fc10f0235442f62",
+    "url": "/static/js/main.8ebb7e20.chunk.js"
+  },
+  {
+    "revision": "ebfe78bb80b70084c45b",
+    "url": "/static/js/runtime-main.5de5fce0.js"
+  }
+]);
